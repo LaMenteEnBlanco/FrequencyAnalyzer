ceab9529d906265c8534e8e00d37ccee30891f8c
LaMenteEnBlanco <jakobschreiber@gmx.de>
2018-09-04 15:44:43 +0200

    I changed the way to put together the frames string. Instead of a list, frames is now a string. The unpack method can now encode the frames-string

--- eclipse-workspace/erstesProjekt/src/FftOfMicSig.py
+++ eclipse-workspace/erstesProjekt/src/FftOfMicSig.py
@@ -26,14 +26,19 @@ stream = audio.open(format=FORMAT, channels=CHANNELS,
                 rate=RATE, input=True,
                 frames_per_buffer=CHUNK)
 print "recording..."
-frames = []
+frames = ''
  
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
-    frames.append(data)
+    print(len(data))
+    print(type(data))
+    frames = frames + data
+    print(len(frames))
 print "finished recording"
  
- 
+print(len(frames))
+print(type(frames))
+
 # stop Recording
 stream.stop_stream()
 stream.close()
@@ -46,6 +51,16 @@ waveFile.setframerate(RATE)
 waveFile.writeframes(b''.join(frames))
 waveFile.close()
 
+#
+nb_samples = 256
+chans = 1 #??
+samps = len(frames)
+sampwidth = 1 #??
+datatype = '<{}{}'.format(samps, {1:'b',2:'h',3:'i',4:'i',8:'q'}[sampwidth])
+
+#
+frame = struct.unpack(datatype, frames)
+
 #N und T wurden durch ausprobieren ermittelt
 # Number of samplepoints
 N = 44100*0.5
@@ -53,7 +68,7 @@ N = 44100*0.5
 T = 1.0 / 2700
 
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-yf = rfft(frames)
+yf = rfft(frame)
 
 fig, ax = plt.subplots()
 ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
