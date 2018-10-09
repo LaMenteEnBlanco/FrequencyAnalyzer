84625c0e32306e61dfb2e1dacde2df4e949c2991
LaMenteEnBlanco <jakobschreiber@gmx.de>
2018-10-09 11:25:39 +0200

    comments added

--- eclipse-workspace/erstesProjekt/src/LowestFreq.py
+++ eclipse-workspace/erstesProjekt/src/LowestFreq.py
@@ -1,7 +1,14 @@
 '''
 Created on 03.10.2018
 
-@author: jakob
+@author: jakob 
+THis class needs two arrays from an fft-analysis-result. One is the x-Axis which contains 
+the frequenzies. The other is the y-Axis which contains the corresponding intensity to each 
+frequenzy.
+The method lowestFreq detects frequenzy peaks in the spectrum. It
+detects the peak with the smallest frequenzy above 60 Hz, that holds an intensity greater than
+40000 units.
+It returns the frequenzy value of the detected peak in the intensity spectrum. 
 '''
 
 class LowestFreq:
@@ -10,16 +17,17 @@ class LowestFreq:
     '''
 
 
-    def __init__(self, yfAbs, xf):
+    def __init__(self, intensityArray, frequenzyArray):
         '''
         Constructor
         '''
-        self.yfAbs = yfAbs.tolist()
-        self.xf = xf.tolist()
+        #
+        self.intensityArray = intensityArray.tolist()
+        self.frequenzyArray = frequenzyArray.tolist()
     
     def lowestFreq(self):
-        for i in range(0, len(self.yfAbs)):
+        for i in range(0, len(self.intensityArray)):
            print(i)
-           if ((self.yfAbs[i]>40000) and (i > 60)):
-               return self.xf[i]
+           if ((self.intensityArray[i]>40000) and (i > 60)):
+               return self.frequenzyArray[i]
             
