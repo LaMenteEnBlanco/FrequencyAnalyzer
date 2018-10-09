05f8c61ec250291c90a430de8f7c38aa916910ea
LaMenteEnBlanco <jakobschreiber@gmx.de>
2018-10-09 11:27:15 +0200

    comments added and rearranged

--- eclipse-workspace/erstesProjekt/src/FftOfMicSigContinuous.py
+++ eclipse-workspace/erstesProjekt/src/FftOfMicSigContinuous.py
@@ -2,6 +2,16 @@
 Created on 03.10.2018
 
 @author: jakob
+This program captures the present microphone signal. It analyses the signal using fft-analyse and then 
+detects the lowest formant in the spectrum above 60 Hz. The resulting frequenzy is printed out every half second.
+Quelle: https://gist.github.com/mabdrabo/8678538#file-sound_recorder-py-L24 (how to record sound from mic)
+Quelle 2: https://stackoverflow.com/questions/25735153/plotting-a-fast-fourier-transform-in-python (How to plot an fft
+especially for me: how to treat the fft results)
+Quelle 3: https://stackoverflow.com/questions/6951046/pyaudio-help-play-a-file (How to play a file with pyAudio. Helpful for me:
+it is similar to import an audio file, which I needed to test the fft)
+Quelle 4: https://stackoverflow.com/questions/19709018/convert-3-byte-stereo-wav-file-to-numpy-array
+(Converting Wavfile into numpy array)
+
 '''
 
 import pyaudio
@@ -14,25 +24,29 @@ from DataAnalyse import DataAnalyse
 from FftAnalyse import FftAnalyse
 from LowestFreq import LowestFreq
  
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 0.5
+# Record Parameters 
+format = pyaudio.paInt16
+channels = 1
+frameRate = 44100
+chunkSize = 1024
+recSeconds = 0.5
  
 while True:
     audio = pyaudio.PyAudio()
- 
+
     # start Recording
-    stream = audio.open(format=FORMAT, channels=CHANNELS,
-                rate=RATE, input=True,
-                frames_per_buffer=CHUNK)
+    stream = audio.open(format=format, channels=channels,
+                rate=frameRate, input=True,
+                frames_per_buffer=chunkSize)
     print "rec..."
-    frames = ''
- 
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames = frames + data
+    
+    # recFrames will include the recorded audio frames
+    recFrames = ''
+    
+    # adding the single chunks of frame to the recFrames string
+    for i in range(0, int(frameRate / chunkSize * recSeconds)):
+        data = stream.read(chunkSize)
+        recFrames = recFrames + data
     
     print "fin"
  
@@ -41,14 +55,13 @@ while True:
     stream.close()
     audio.terminate()
 
-    #nb_samples = 256
-    chans = 1 #??
-    samps = len(frames)
+    #converting the recFrames-string into a list
+    samps = len(recFrames)
     sampwidth = 1 #??
     datatype = '<{}{}'.format(samps, {1:'b',2:'h',3:'i',4:'i',8:'q'}[sampwidth])
-    frameList = struct.unpack(datatype, frames)
+    frameList = struct.unpack(datatype, recFrames)
 
-    #Analyse and Plotting
+    #Analyse and Printing
     fftAnalyse = FftAnalyse(frameList)
     lowestFreq = LowestFreq(fftAnalyse.fftAnalyse(), fftAnalyse.getXf())
     print(lowestFreq.lowestFreq())
